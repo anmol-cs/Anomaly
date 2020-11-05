3844ef2496cb23857d8959f41d3fbd301d71317c
anmol-cs <anmolsingh9822@gmail.com>
2020-11-06 03:03:35 +0530

    Replaced the heroku local repository name with origin and planning to link heroku directly to github

--- app.py
+++ app.py
@@ -6,45 +6,41 @@ import requests
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS']=False
+findic={}
 ##SOLUTION 1 COUNTRY REST API##
 ##TASK 1:-GET COUNTRY BY NAME##
-@app.route('/country/name/', methods=['GET'])
-def freegeoip():
-    name=request.args['name']
-    print(name)
-    url="https://restcountries.eu/rest/v2/name/"+name.strip()+"?fullText=true"
-    url_request=requests.get(url)
-    json_object=url_request.json()
-    findic={}
-    findic["name"]=json_object[0]["name"]
-    findic["alpha2Code"]=json_object[0]["alpha2Code"]
-    findic["alpha3Code"]=json_object[0]["alpha2Code"]
-    findic["capital"]=json_object[0]["capital"]
-    findic["region"]=json_object[0]["region"]
-    findic["population"]=json_object[0]["population"]
-    findic["flag"]=json_object[0]["flag"]
-    findic["totalLanguages"]=count_dic(json_object[0]["languages"])
-    findic["totalCurrencies"]=count_dic(json_object[0]["currencies"])
+@app.route('/country/name/<name>', methods=['GET'])
+def freegeoip(name):
+    obj="https://restcountries.eu/rest/v2/name/"+name.strip()+"?fullText=true"
+    obj=requests.get(obj)
+    obj=obj.json()
+    findic["name"]=obj[0]["name"]
+    findic["alpha2Code"]=obj[0]["alpha2Code"]
+    findic["alpha3Code"]=obj[0]["alpha2Code"]
+    findic["capital"]=obj[0]["capital"]
+    findic["region"]=obj[0]["region"]
+    findic["population"]=obj[0]["population"]
+    findic["flag"]=obj[0]["flag"]
+    findic["totalLanguages"]=count_dic(obj[0]["languages"])
+    findic["totalCurrencies"]=count_dic(obj[0]["currencies"])
     return jsonify(findic)
 
 #Task 2: Get country by Code
 @app.route('/country/code/<code>', methods=['GET'])
 def free(code):
-  url="https://restcountries.eu/rest/v2/alpha/"+code
-  print(url)
-  url_request=requests.get(url)
-  json_object=url_request.json()
-  findic={}
-  findic["name"]=json_object["name"]
-  findic["alpha2Code"]=json_object["alpha2Code"]
-  findic["alpha3Code"]=json_object["alpha3Code"]
-  findic["capital"]=json_object["capital"]
-  findic["region"]=json_object["region"]
-  findic["population"]=json_object["population"]
-  findic["flag"]=json_object["flag"]
-  findic["totalLanguages"]=count_dic(json_object["languages"])
-  findic["totalCurencies"]=count_dic(json_object["currencies"])
-  findic["totalTimezones"]=len(json_object["timezones"])
+  obj="https://restcountries.eu/rest/v2/alpha/"+code
+  obj=requests.get(obj)
+  obj=obj.json()
+  findic["name"]=obj["name"]
+  findic["alpha2Code"]=obj["alpha2Code"]
+  findic["alpha3Code"]=obj["alpha3Code"]
+  findic["capital"]=obj["capital"]
+  findic["region"]=obj["region"]
+  findic["population"]=obj["population"]
+  findic["flag"]=obj["flag"]
+  findic["totalLanguages"]=count_dic(obj["languages"])
+  findic["totalCurencies"]=count_dic(obj["currencies"])
+  findic["totalTimezones"]=len(obj["timezones"])
   return jsonify(findic)
 
 def count_dic(dicto):
